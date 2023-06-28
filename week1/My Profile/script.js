811307062e3d98797dc1738714465cdad4d24998
diegocepedap <130510278+diegocepedap@users.noreply.github.com>
2023-06-28 12:00:07 -0700

    Add files via upload

---
+++ week1/My Profile/script.js
@@ -0,0 +1,6 @@
+let firstName = "Diego";
+let lastName = "Cepeda";
+let monthsOfStudy = 4;
+let goal = "finish the Year Up program with the best knowledge and be able to put it into practice in technology companies.";
+
+
